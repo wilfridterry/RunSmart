968a2cccf58e2521baa555885d180d9b00258661
Stannis <zalevschi1998@gmail.com>
2022-05-26 22:28:46 +0300

    Started layout

--- gulpfile.js
+++ gulpfile.js
@@ -20,10 +20,7 @@ gulp.task('styles', () => {
                 prefix: "",
                 suffix: ".min"
             }))
-            .pipe(autoprefixer({
-                browsers: ['last 2 versions'],
-                cascade: false
-            }))
+            .pipe(autoprefixer())
             .pipe(cleanCSS({compatibility: 'ie8'}))
             .pipe(gulp.dest('src/css'))
             .pipe(browserSync.stream());
