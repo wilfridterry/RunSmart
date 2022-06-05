697f388fb2f85202e5b9d17f66548342d76caeb5
Stannis <zalevschi1998@gmail.com>
2022-06-05 22:21:56 +0300

    Fixed modal js logic

--- src/sass/blocks/form/form.js
+++ src/sass/blocks/form/form.js
@@ -10,7 +10,7 @@ function validateForm(selector) {
             email: {
                 required: true,
                 email: true
-            }
+            }           
         }
     });
 }
--- src/sass/blocks/modal/modal.js
+++ src/sass/blocks/modal/modal.js
@@ -2,43 +2,50 @@ document.addEventListener('click', e => {
     const toggleModal = e.target.dataset.toggleModal;
     if (toggleModal != undefined) {
         const modal = document.querySelector(`#${toggleModal}`);
-
-        document.body.style.overflow = 'hidden';
-        document.querySelector('.overlay').style.display = 'block';
-        
-        modal.style.display = 'block';
-        modal.style.opacity = '0';
-
-        setTimeout(() => {
-            modal.style.opacity = '1';
-        }, 0);        
-        
-        for (let prop in e.target.dataset) {
-            if (prop.startsWith('modal:')) {
-                const elem = prop.split(':')[1];
-                modal.querySelector(`.modal__${elem}`).innerHTML = e.target.dataset[prop];
-            }
-        }
+        openModal(modal);
     }
 });
 
 document.addEventListener('click', (e) => {
     if (e.target.classList.contains('modal__close')) {
-        document.body.style.overflow = '';
-        document.querySelector('.overlay').style.display = 'none';
-        
-        const modal = e.target.parentElement;
-        modal.style.display = 'none';
-        
-        const form = modal.querySelector('form');
-        if (form != undefined) {
-            form.reset();
-            form.querySelectorAll('.error').forEach(elem => {
-                elem.classList.remove('error');
-                if (elem.tagName == "LABEL") {
-                    elem.remove();
-                }
-            });
-        } 
+        closeModal(e.target.parentElement)
     }
 });
+
+
+function openModal(modal) {
+    document.body.style.overflow = 'hidden';
+    document.querySelector('.overlay').style.display = 'block';
+    
+    modal.style.display = 'block';
+    modal.style.opacity = '0';
+
+    setTimeout(() => {
+        modal.style.opacity = '1';
+    }, 0);        
+    
+    for (let prop in e.target.dataset) {
+        if (prop.startsWith('modal:')) {
+            const elem = prop.split(':')[1];
+            modal.querySelector(`.modal__${elem}`).innerHTML = e.target.dataset[prop];
+        }
+    }
+}
+
+function closeModal(modal) {
+    document.body.style.overflow = '';
+    document.querySelector('.overlay').style.display = 'none';
+    
+    modal.style.display = 'none';
+    
+    const form = modal.querySelector('form');
+    if (form != undefined) {
+        form.reset();
+        form.querySelectorAll('.error').forEach(elem => {
+            elem.classList.remove('error');
+            if (elem.tagName == "LABEL") {
+                elem.remove();
+            }
+        });
+    } 
+}
