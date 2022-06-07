4dbd7d96b00d0c0058277395983a699780e43f9f
Stannis <zalevschi1998@gmail.com>
2022-06-07 23:45:34 +0300

    Finished with the project

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,12 @@ const sass = require('gulp-sass')(require('sass'));
 const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('server', () => {
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
 });
@@ -22,19 +23,41 @@ gulp.task('styles', () => {
             }))
             .pipe(autoprefixer())
             .pipe(cleanCSS({compatibility: 'ie8'}))
-            .pipe(gulp.dest('src/css'))
+            .pipe(gulp.dest('dist/css'))
             .pipe(browserSync.stream());
 });
 
 gulp.task('scripts', () => {
     return gulp.src("src/sass/**/*.js")
-            .pipe(gulp.dest('src/js'))
+            .pipe(gulp.dest('dist/js'))
             .pipe(browserSync.stream());
 });
 
 gulp.task('watch', () => {
-    gulp.watch("src/sass/**/*.+(scss|sass|js)", gulp.parallel('styles', 'scripts'));
-    gulp.watch('src/*.html').on("change", browserSync.reload);
+    gulp.watch("src/sass/**/*.+(scss|sass|js|css)", gulp.parallel('styles', 'scripts'));
+    gulp.watch('src/*.html').on("change", gulp.parallel('html'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts'));
+gulp.task('html', () => {
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('fonts', () => {
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task('icons', () => {
+    return gulp.src("src/icons/**/*")
+        .pipe(gulp.dest("dist/icons"));
+});
+
+gulp.task('images', () => {
+    return gulp.src("src/img/**/*")
+        // .pipe(imagemin())
+        .pipe(gulp.dest("dist/img/"));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'images', 'html'));
